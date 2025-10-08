a5edc163025864321d7a36a7a8d4e884bc3a628d
RenatoGhirelli <79719528+RenatoGhirelli@users.noreply.github.com>
2025-10-07 23:57:50 -0300

    Ajuste primeiros testes postman

---
+++ src/main/java/br/com/fiap/techchallenge2/infra/datainit/TipoUsuarioDataLoader.java
@@ -0,0 +1,23 @@
+package br.com.fiap.techchallenge2.infra.datainit;
+
+import br.com.fiap.techchallenge2.infra.model.TipoUsuarioModel;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import br.com.fiap.techchallenge2.infra.repository.TipoUsuarioRepository;
+
+@Component
+public class TipoUsuarioDataLoader {
+
+    @Autowired
+    private TipoUsuarioRepository tipoUsuarioRepository;
+
+    @PostConstruct
+    public void init() {
+        if (tipoUsuarioRepository.count() == 0) {
+            TipoUsuarioModel tipoUsuario = new TipoUsuarioModel();
+            tipoUsuario.setNome("Admin");
+            tipoUsuarioRepository.save(tipoUsuario);
+        }
+    }
+}
