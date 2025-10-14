69ae4d2a87bf501dce25fe4fc4407083dd79c809
Anderson Lima <andclima@gmail.com>
2025-10-14 00:16:07 -0300

    Correções no teste do controller de tipo de usuário.

--- src/test/java/br/com/fiap/techchallenge2/application/controller/TipoUsuarioControllerTest.java
+++ src/test/java/br/com/fiap/techchallenge2/application/controller/TipoUsuarioControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
